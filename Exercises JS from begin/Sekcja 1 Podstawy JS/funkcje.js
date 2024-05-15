1b12171d7461d722141a399a051d3374a48b05e0
iska000s <izabelast@op.pl>
2024-05-15 12:12:56 +0200

    Zadania z funkcjami

--- Exercises JS from begin/Sekcja 1 Podstawy JS/funkcje.js
+++ Exercises JS from begin/Sekcja 1 Podstawy JS/funkcje.js
@@ -92,4 +92,90 @@
     if (123) console.log("Wykona się7");
     if (!false) console.log("Wykona się8");
     if (-12) console.log("Wykona się8");
+
+
+let name = "Asia";
+
+if (name === "Asia"){
+    let name = "Anna"   //zmienna lokalna
+    console.log(`Zmiana imienia Asia na ${name}`);
+} else {
+    console.log("błędne imię")
+}
+console.log(name);
+
+
+var name3 = "Bolek";
+
+if (name === "Bolek"){
+    var name3 = "Przemek"   //var nadpisało globalną
+    console.log(`Zmiana imienia Bolek na ${name3}`);
+} else {
+    console.log("błędne imię")
+}
+console.log(name3);
     
+
+function multiply (a, b) {
+    let multiplication = a * b;
+    return multiplication
+    // console.log(multiplication);
+}
+
+
+// multiply(3, 10);
+// multiply(120, 56);
+// multiply(1234, 10);
+console.log( multiply(3, 10) );
+console.log( multiply(1, 2) );
+
+
+function checkName (name){
+    if(name == "Kasia"){
+        return;
+    }
+    else {
+        return "imię się nie zgadza";
+    }
+}
+
+console.log(checkName("Bolek"));
+console.log(checkName("Kasia"));
+
+
+let arr3 = [1, 2, 3, 4];
+
+function averageValue (){
+    let sum = (arr3[0] + arr3[1] + arr3[2] + arr3[3]) / arr3.length
+    console.log(sum);
+}
+
+averageValue(arr3)
+
+
+
+function averageValue2 (a, b, c, d){
+    let sum = (a + b + c + d) / 4
+    return sum;
+}
+
+console.log(averageValue2(1, 5, 5, 5))
+
+
+function hireEmployee(name, surname) {
+    if (name.length < 3) return null;
+    if (surname.length < 3) return null;
+    if (name.length > 11 || typeof name !== "string") return "Błąd";
+    else {
+        return name + " " + surname;
+    }
+    const obj = {
+        name: name,
+        surname: surname
+    };
+    // return obj;
+}
+
+const employee1 = hireEmployee("Kasia", "Kowalska");
+console.log(employee1)
+
