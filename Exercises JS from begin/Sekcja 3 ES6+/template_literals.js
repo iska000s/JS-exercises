f548f426a61816cbffa68a269206c7c5e429f809
iska000s <izabelast@op.pl>
2024-06-20 15:37:45 +0200

    New methods ES6 - string

---
+++ Exercises JS from begin/Sekcja 3 ES6+/template_literals.js
@@ -0,0 +1,61 @@
+const user = {name: "Ania", city: "Wawa"};
+const txt = `
+Hello ${user.name} from ${user.city}.
+${2+2}
+`;
+
+console.log(txt);
+
+
+
+// korzystanie z tagów w literałach - funkcja, która jest preprocesowrem - mamy wpływ jak osadzane są wartości
+
+function uppercase(strings, ...values) {
+    console.log(strings);
+    console.log(values);
+
+    let result = " ";
+    for (let i = 0; i <strings.length; i++){
+        result += strings[i];
+        if (i < values.length){
+            let v = values[i];
+            if (typeof v === "string") v = v.toUpperCase();
+            result += v;
+    }}
+    return result;
+};
+
+console.dir("PRZYKŁAD 1")
+
+const txt2 = uppercase`
+Hello ${user.name} from ${user.city}.
+${2+2}
+`;
+
+console.log(txt2);
+
+console.dir("PRZYKŁAD 2")
+
+const txt3 = uppercase(`
+Hello ${user.name} from ${user.city}.
+${2+2}
+`);
+
+console.log(txt2);
+
+
+// nowe metody w string
+
+const text = "Hello world!";
+console.log(text.includes("world")) //true
+console.log(text.startsWith("Hello")) //true - jeżeli zaczyna się tym tekstem
+
+
+
+
+
+
+
+
+
+
--- Exercises JS from begin/app.js
+++ Exercises JS from begin/app.js
@@ -1,21 +1,5 @@
 
-// import './Sekcja 1 Podstawy JS/funkcje.js';
-// import './Sekcja 1 Podstawy JS/operatory.js';
-// import './Sekcja 1 Podstawy JS/funkcje_spread.js';
-// import './Sekcja 1 Podstawy JS/funckej czasu.js';
-// import './Sekcja 1 Podstawy JS/tablice.js';
-
-// import './Sekcja 2 Zaawansownay JS/metody_obiektu.js';
-// import './Sekcja 2 Zaawansownay JS/zaawansowany_js_1.js';
-// import './Sekcja 2 Zaawansownay JS/drzewo_dom.js';
-// import './Sekcja 3 ES6+/arrow_functions.js';
-// import './Sekcja 3 ES6+/spread_rest.js';
-// import './Sekcja 3 ES6+/destukturyzujące_przypisanie.js';
-// import './Sekcja 3 ES6+/for_of_in.js';
-// import './Sekcja 3 ES6+/map.js';
-// import './Sekcja 3 ES6+/class.js';
-// import './Sekcja 3 ES6+/promise.js';
-import './Sekcja 3 ES6+/default_param_values.js';
+import './Sekcja 3 ES6+/template_literals.js';
 
 // console.log(window);
 // alert("Otwórz magazyn");
