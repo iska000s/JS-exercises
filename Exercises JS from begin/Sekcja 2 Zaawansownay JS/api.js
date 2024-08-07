fa8d1264237b7e0361d0e8b93c54a64523be9750
iska000s <izabelast@op.pl>
2024-08-07 16:45:14 +0200

    API

--- Exercises JS from begin/Sekcja 2 Zaawansownay JS/api.js
+++ Exercises JS from begin/Sekcja 2 Zaawansownay JS/api.js
@@ -1,14 +1,56 @@
-const url = "https://swapi.dev/api/people/1/";
+const url = "https://swapi.dev/api";
 
-fetch(url)
-.then(response => response.json())
-.then(heightPerson)
-.then(() => console.log("TEST"))
+// fetch(url)
+// .then(response => response.json())
+// .then(heightPerson)
+// .then(() => console.log("TEST"))
+// .catch( err => {
+//     console.log('Błąd!', err);
+//    });
+   
 
-function heightPerson(data){
-    let height = Number(data.height);
-    let hat = 10;
-    let sum = height + hat;
-    
-    console.log(sum, data);
+// fetch(url, {
+// method: "GET",
+// // body: JSON.stringify(),
+// headers: {
+//     "Content-Type": "application/json"
+// }
+// })
+// .then(response => response.json())
+// .then(heightPerson)
+// .then(() => console.log("TEST"))
+// .catch( err => {
+//     console.log('Błąd!', err);
+// });
+   
+
+
+// function heightPerson(data){
+//     let height = Number(data.height);
+//     let hat = 10;
+//     let sum = height + hat;
+//     console.log(`Person 1 - ${data.name}, height with a hat: ${sum}`);
+//     console.log("Person 1 - information: ", data);
+// }
+
+
+const newPerson = {
+    name: "Basia",
+    height: 165,
 }
+
+fetch(url, {
+    method: "POST",
+    body: JSON.stringify(newPerson),
+    headers: {
+        "Content-Type": "application/json"
+    }
+})
+.then(response => response.json())
+.then(data => {
+    console.log(newPerson);
+    console.log(data);
+})
+.catch(error => {
+    console.log(error);
+});
