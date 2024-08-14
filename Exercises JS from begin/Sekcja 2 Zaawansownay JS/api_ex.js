8db522302df9148fe23885fa94b60e18f215588b
iska000s <izabelast@op.pl>
2024-08-14 18:19:14 +0200

    API

--- Exercises JS from begin/Sekcja 2 Zaawansownay JS/api_ex.js
+++ Exercises JS from begin/Sekcja 2 Zaawansownay JS/api_ex.js
@@ -1,17 +1,35 @@
-const url = "https://api.chucknorris.io/jokes/random";
+// const url = "https://api.chucknorris.io/jokes/random";
+
+// window.onload = function (){
+//     let div = document.querySelector("#joke");
+//     setInterval(function() {
+//         getJoke(function(data) {
+//             console.log(data);
+//             div.innerHTML = data.value;
+//         });
+//     }, 2000);
+// }
+
+// function getJoke (callback){
+//     fetch(url)
+//     .then(response => response.json())
+//     .then(callback);
+// }
+
+
+
+const url2 = "https://api.chucknorris.io/jokes/random";
 
 window.onload = function (){
-    let div = document.querySelector("#joke");
-    setInterval(function() {
-        getJoke(function(data) {
-            console.log(data);
-            div.innerHTML = data.value;
-        });
-    }, 2000);
+    let div2 = document.querySelector("#joke");
+    getJoke2(function(data){
+        console.log(data);
+        div2.innerHTML = data.value;
+    });
 }
 
-function getJoke (callback){
-    fetch(url)
-    .then(response => response.json())
-    .then(callback);
+function getJoke2 (callback){
+        fetch(url2)
+        .then(response => response.json())
+        .then(callback);
 }
