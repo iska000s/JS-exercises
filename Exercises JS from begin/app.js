6cdd81dced99d3e5a801a5dc166a7c84d81948b2
Izabela Stankiewicz <izabelast@op.pl>
2024-08-29 09:43:53 +0200

    prototype

--- Exercises JS from begin/app.js
+++ Exercises JS from begin/app.js
@@ -4,6 +4,7 @@
 import './01_Zadanie_1/js/promise_zadanie.js/names.js';
 import './Sekcja 2 Zaawansownay JS/api.js';
 import './Sekcja 2 Zaawansownay JS/api_ex.js';
+import './Sekcja 2 Zaawansownay JS/prototype.js';
 
 // import Result from "./Sekcja 3 ES6+/array_functions.js";
 
